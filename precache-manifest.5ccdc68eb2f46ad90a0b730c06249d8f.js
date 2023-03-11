084c28bc71b65cfc2fe8781f77a24a0dbb749d33
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2023-03-11 07:35:58 +0530

    Updates

---
+++ precache-manifest.5ccdc68eb2f46ad90a0b730c06249d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d599c6c74c818cc821c",
+    "url": "/TextUtils-react/static/css/main.ef5d4622.chunk.css"
+  },
+  {
+    "revision": "8d599c6c74c818cc821c",
+    "url": "/TextUtils-react/static/js/main.8d599c6c.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "7d11b97854f6b254e3c1",
+    "url": "/TextUtils-react/static/js/runtime~main.7d11b978.js"
+  },
+  {
+    "revision": "3ffe93826fb651a23720153131a8b4a6",
+    "url": "/TextUtils-react/index.html"
+  }
+];
