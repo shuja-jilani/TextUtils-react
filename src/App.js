065f94520951e481f2afb24f99b3ed42f3a838a0
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2023-03-13 20:38:31 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
   };
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+      
       <Router>
         <Navbar
           title="TextUtils"
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -72,7 +72,7 @@ color: props.mode==='light'? 'black':'white'}} id="myBox" rows="8"></textarea>
 </div>
 <div className="container my-3"  style={{color: props.mode==='dark'?'white':'black'}}>
     <h2>Your text summary</h2>
-    <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p> 
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p> 
     <p>{0.008* (text.split(" ").filter((element)=>{return element.length!==0}).length)} Minutes read </p>
     <h2>Preview</h2>
     <p>{text.length>0?text:"Nothing to preview"}</p>
